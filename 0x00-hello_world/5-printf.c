09b00eed7ada65878818a96f20ef80817ed328b6
CodeSyries <zakeaurora436@gmail.com>
2023-07-16 09:07:58 -0700

    Hello World

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *  main-Entry point
+ *
+ *  Return: Always 0 (success)
+ */
+int main() {
+    printf("%s%s%s%s\n", "Programming", " ", "is", " like building a multilingual puzzle,");
+    printf("%s%s%s%s\n", "but", " ", "the outcome", " is a piece of art,");
+    return 0;
+}
